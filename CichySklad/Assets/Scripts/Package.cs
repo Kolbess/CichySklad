97f36e9b0fd71aacfe26ab7967ed45869a8276a2
mateusz.roszko_infot <roszkomateusz08@gmail.com>
2025-12-08 16:48:03 +0100

    package implementation with item duplication bug

--- CichySklad/Assets/Scripts/Package.cs
+++ CichySklad/Assets/Scripts/Package.cs
@@ -6,6 +6,14 @@ public class Package : MonoBehaviour
     [SerializeField] private Transform dropPoint;
     private List<GameObject> _items = new List<GameObject>();
     public List<GameObject> GetItems() => _items;
+    private SpriteRenderer _spriteRenderer;
+    private Material _material;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _material = _spriteRenderer.material;
+    }
     
     public void AddItem(GameObject g)
     {
@@ -13,9 +21,24 @@ public class Package : MonoBehaviour
         _items.Add(g);
     }
 
+    private void OnMouseEnter()
+    {
+        Utils.EnableOutline(_material);
+    }
+    
+    private void OnMouseExit()
+    {
+        Utils.DisableOutline(_material);
+    }
+
     private void OnMouseDown()
     {
-        if (_items.Count == 0) return;
+        Debug.Log(_items.ToString()+" "+_items.Count);
+        if (_items.Count == 0)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
 
         GameObject item = _items[0];
         _items.RemoveAt(0);
